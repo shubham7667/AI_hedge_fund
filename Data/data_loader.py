be795ac0f6be15803421237fef0597f0f836cac3
Shubham Kumar <rishab.ss64@gmail.com>
2026-07-14 03:54:27 +0530

    Add Data module

---
+++ Data/data_loader.py
@@ -0,0 +1,25 @@
+import os
+import time
+import yfinance as yf
+
+def load_data():
+    day = time.localtime().tm_mday
+
+    data = yf.download(
+        "AMZN",
+        start="2016-11-10",
+        end="2026-07-10"
+    )
+
+    # Get the Data folder path
+    data_folder = os.path.dirname(__file__)
+
+    # Create the full path
+    filename = os.path.join(
+        data_folder,
+        f"stock_data_on_date_{day}.csv"
+    )
+
+    data.to_csv(filename, index=False)
+
+    return data
