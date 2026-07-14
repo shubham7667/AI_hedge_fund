4457215fd091e72382afb47862e3cdebdf685aea
Shubham Kumar <rishab.ss64@gmail.com>
2026-07-15 02:34:00 +0530

    price-feature

--- Data/data_loader.py
+++ Data/data_loader.py
@@ -1,13 +1,14 @@
 import os
 import time
 import yfinance as yf
-def load_data():
+def load_weekly_data():
     day = time.localtime().tm_mday
 
     data = yf.download(
         "AMZN",
         start="2016-11-10",
-        end="2026-07-10"
+        end="2026-07-10",
+        interval='1wk'
     )
 
     # Get the Data folder path
@@ -21,4 +22,21 @@ def load_data():
 
     data.to_csv(filename, index=False)
 
+    return data
+
+def load_daily_data():
+    day = time.localtime().tm_mday
+    data = yf.download(
+        "AMZN",
+        start="2016-11-10",
+        end="2026-07-10",
+        interval='1d'
+    )
+    
+    data_folder = os.path.dirname(__file__)
+    file_name = os.path.join(
+        data_folder,
+        f"stock_daily_data_on_data{day}.csv"
+    )
+    data.to_csv(file_name,index=False)
     return data
